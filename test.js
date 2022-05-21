5f873630a0bf8feee83773191742c2d085b18f07
junwangshuijun <50397690+junwangshuijun@users.noreply.github.com>
2022-05-21 21:30:08 +0800

    Update test.js

--- test.js
+++ test.js
@@ -49,7 +49,7 @@
   runCurl = function() {
     var curl;
     curlRunning = true;
-    curl = child_process.spawn('curl', ['-v', 'http://www.example.com/', '-L', '--socks5-hostname', '127.0.0.1:8843']);
+    curl = child_process.spawn('curl', ['-v', 'http://www.example.com/', '-L', '--socks5-hostname', '127.0.0.1:8856']);
     curl.on('exit', function(code) {
       local.kill();
       server.kill();
